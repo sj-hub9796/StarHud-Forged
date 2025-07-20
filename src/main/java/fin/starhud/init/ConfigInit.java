1b141c2e20832050791f284e0c2935f10d2bc5e4
sjhub <sj.hub9796@gmail.com>
2025-07-20 17:55:19 +0900

    use native mod settings

--- src/main/java/fin/starhud/init/ConfigInit.java
+++ src/main/java/fin/starhud/init/ConfigInit.java
@@ -7,6 +7,9 @@ import me.shedaniel.autoconfig.AutoConfig;
 import me.shedaniel.autoconfig.ConfigHolder;
 import me.shedaniel.autoconfig.serializer.GsonConfigSerializer;
 import net.minecraft.world.InteractionResult;
+import net.minecraftforge.client.ConfigScreenHandler;
+import net.minecraftforge.fml.IExtensionPoint;
+import net.minecraftforge.fml.ModLoadingContext;
 
 public class ConfigInit {
     public static void init() {
@@ -21,5 +24,13 @@ public class ConfigInit {
             HUDComponent.getInstance().updateAll();
             return InteractionResult.SUCCESS;
         });
+
+        // starhudforged - use native mod settings instead modmenu
+        ModLoadingContext.get().registerExtensionPoint(
+                ConfigScreenHandler.ConfigScreenFactory.class,
+                () -> new ConfigScreenHandler.ConfigScreenFactory(((minecraft, screen) -> {
+                    return AutoConfig.getConfigScreen(Settings.class, screen).get();
+                }))
+        );
     }
 }
